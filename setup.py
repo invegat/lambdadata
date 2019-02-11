069272f9b3b3e75e0eb1f625323dc5bdadd58467
Mark Oliver <mark.c.oliver@gmail.com>
2019-02-11 18:56:02 -0500

    more 400 error

--- setup.py
+++ setup.py
@@ -13,8 +13,8 @@ with open("README.md", "r") as fh:
     LONG_DESCRIPTION = fh.read()
 
 setuptools.setup(
-    name="lambdadata_invegat",
-    version="0.0.5",
+    name="lambdadata_invegat2",
+    version="0.0.6",
     author="invegat",
     description="A collection of Data Science helper functions",
     long_description=LONG_DESCRIPTION,
