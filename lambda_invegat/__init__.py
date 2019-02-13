a63659c8d6c006b89bd1a812069a925737878a57
Mark Oliver <mark.c.oliver@gmail.com>
2019-02-12 21:42:17 -0500

    added compare_linear_xgboost

--- lambda_invegat/__init__.py
+++ lambda_invegat/__init__.py
@@ -1,11 +1,11 @@
-HELLO="Hello"
 """ lambdata - a Data Science Helper
 """
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_classification
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.linear_model import LogisticRegression
+from mlxtend.plotting import plot_decision_regions
 import numpy as np
-import pandas as pd
-
-VERSION = 0
-ONES = np.ones(100)
-ONES_DF = pd.DataFrame(ONES)
-
+import xgboost as xgb
 
